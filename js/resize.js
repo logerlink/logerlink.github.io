18610e8bf7cf71cfecd5e7cd90b1f35827dbb432
DESKTOP-5I2U4EC\loger <logerlink@outlook.com>
2021-05-17 09:53:13 +0800

    修改目录展示形式

--- js/resize.js
+++ js/resize.js
@@ -35,3 +35,28 @@ export function resizeHandler(){
         if(article !== '') article.style.margin = '20px'
     }
 }
+
+export function toggleTOC(){
+    let btn = document.querySelector('.btn-toggle') || ''
+    if(btn != ''){
+        btn.addEventListener('click',()=>{
+            let text = btn.innerText
+            let mdToc = document.querySelector('.md-toc') || ''
+            if(text === '>'){
+                //收起
+                if(mdToc != '') {
+                    mdToc.classList.add('toc-hidden')
+                    mdToc.style.right = `-${mdToc.offsetWidth - 20}px`
+                    btn.innerText = '<'
+                }
+            }else{
+                if(mdToc != '')
+                {
+                    mdToc.classList.remove('toc-hidden')
+                    mdToc.style.right = '0'
+                    btn.innerText = '>'
+                }
+            }
+        })
+    }
+}
