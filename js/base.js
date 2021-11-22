30f6578aa7697f635b8b22647d741c68edc887d8
DESKTOP-5I2U4EC\loger <logerlink@outlook.com>
2021-11-22 13:51:49 +0800

    图片懒加载、放大功能

--- js/base.js
+++ js/base.js
@@ -1,10 +1,17 @@
 import {initCategory,initCategoryList,initTitleCate} from './article.js'
 import {toggleTOC,toTop} from './resize.js?v=2'
+import {showImage,lazyLoad} from './image.js'
 
 window.onload = function(){
+    let imgs = document.querySelectorAll('img');
+    lazyLoad(imgs);
+    showImage(imgs);
     initTitleCate();
     toTop();
     toggleTOC();
     initCategory();
     initCategoryList();
+    window.onscroll = function(){
+        lazyLoad(imgs);
+    }
 }
